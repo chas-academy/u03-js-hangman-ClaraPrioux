60053f846fc1ccd118d0e568c3dbfc4d3e290667
Clara Prioux <prioux.clara@gmail.com>
2023-11-27 18:32:44 +0100

    Start game button + word generate + letter listener + fixes #3 + fixes #4 + need to create more issues

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Globala variabler
 
-const wordList = ["Elefant", "Ugglor", "Fåtölj", "Ballong", "Fönster", "Mamma", "Papper", "Frukost", "Skratta", "Sköldpadda"]; // Array: med spelets alla ord
-let selectedWord;    // Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
+ // Array: med spelets alla ord
+let wordToGuess;    // Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
 
 let guesses = 0;     // Number: håller antalet gissningar som gjorts
 let hangmanImg;      // Sträng: sökväg till bild som kommer visas (och ändras) fel svar. t.ex. `/images/h1.png`
@@ -24,7 +24,7 @@ let letterBoxEls;    // Array av DOM-noder: Rutorna där bokstäverna ska stå
 
 // Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
 
-
+const wordList = ["Elefant", "Ugglor", "Fåtölj", "Ballong", "Fönster", "Mamma", "Papper", "Frukost", "Skratta", "Sköldpadda"];
 document.addEventListener('DOMContentLoaded', function () {
     const startGameBtn = document.getElementById('startGameBtn');// DOM-nod: knappen som du startar spelet med
     const gameBoard = document.getElementById('gameBoard');
@@ -33,7 +33,9 @@ document.addEventListener('DOMContentLoaded', function () {
     function startGame() {
         // Show the gameBoard section
         gameBoard.style.display = 'block';
-        generateRandomWord();
+        wordToGuess = generateRandomWord();
+        createLetterBoxes(wordToGuess);
+        this.disabled = true;
         // console.log(generateRandomWord());
     }
     // Attach the function to the button's click event
@@ -42,15 +44,35 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // A function to generate the word randomly, that will be called by the startGame button 
 function generateRandomWord() {
-    let wordToGuess = wordList[(Math.floor(Math.random() * wordList.length))];
-    createLetterBoxes(wordToGuess);
-    return wordToGuess;
+    return wordList[(Math.floor(Math.random() * wordList.length))];
 }
 
-function createLetterBoxes(wordInBoxes) {
-    numberLetters = wordInBoxes.length;
-    console.log(numberLetters);
-    for(let i = 0; i < createLetterBoxes; i ++){
-        // CONTINUE HERE
+// A function to create boxes for the letter to guess (number of boxes = wordToGuess.length)
+function createLetterBoxes() {
+    let numberLetters = wordToGuess.length;
+    let letterBoxes = document.getElementById("letterBoxes");
+    letterBoxes.innerHTML = "";
+    for(let i = 0; i < numberLetters; i ++){
+        let input = document.createElement("input");
+        letterBoxes.appendChild(input);
       }
 }
+
+// When click on a letter button => event
+const button = document.querySelector("#letterButtons");
+
+button.addEventListener("click", (event) => {
+    const buttonValue = event.target.value;
+    compareLetters(buttonValue);
+});
+
+// function that compare the letter selected with the letters from the wordToGuess
+function compareLetters(buttonValue) {
+    for (let i = 0; i < wordToGuess.length; i++) {
+        if (buttonValue == wordToGuess[i].toUpperCase()) {
+            console.log(`Button value ${buttonValue} matches letter ${wordToGuess[i]}`);
+        }
+    }
+} 
+
+// Next step to display it in the boxes
